441de30e134c28e293d792d379835d04193560b4
Joe Longstreet <joelongstreet@gmail.com>
2019-03-13 22:15:22 -0500

    implement material design css baseline

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Tile from './Tile'
 import Settings from './Settings'
 import SettingsProvider from './Settings.provider';
 
 function App() {
-  return (    
+  return (
     <SettingsProvider>
       <React.Fragment>
+        <CssBaseline />
         <Grid container spacing={24}>
           <Grid item xs={8}>
             <Tile />
