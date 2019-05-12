eab51075981b51ad6f7b47829d68e9c119f21ab8
EliTheCoder <ex.games.org@gmail.com>
2019-05-12 15:41:00 -0500

    added download warning - closes #11

--- src/Settings-page.js
+++ src/Settings-page.js
@@ -8,6 +8,7 @@ import {
   Row,
   Select,
   Tooltip,
+  notification,
 } from 'antd';
 
 import * as Styles from './Styles';
@@ -73,6 +74,12 @@ function downloadSvgDocument() {
   const file = `data:application/octet-stream;base64,${window.btoa(svgDocument)}`;
 
   const timestamp = dayjs().format('YYYY-MM-DDTHH:mm');
+
+  notification.warning({
+    message: 'Warning',
+    description: 'DataForms outputs cut files as SVG. Before sending to laser cutter, change the document settings to standard or metric units with 72 DPI. If the document is setup properly, the document dimensions in the vector graphics editor will match the dimensions listed on the page tab from DataForms. Cut line strokes will be set to 0.001 inches if using standard units, etch lines set to the specified etch width in DataForms.',
+    duration: 0,
+  });
 
   const aLink = document.createElement('a');
   aLink.download = `DataForms ${timestamp}.svg`;
