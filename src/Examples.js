2a03227f67f72a6e2c67311fbdbb048eac88c616
Joe Longstreet <joelongstreet@gmail.com>
2019-05-15 20:13:52 -0500

    create an image carousel for each example

--- src/Examples.js
+++ src/Examples.js
@@ -1,52 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
 import * as Styles from './Styles';
 import ExampleData from './Examples.data';
 import Example from './Example';
 
 
-function Examples(props) {
-  const { visible, onClose } = props;
+class Examples extends Component {
+  state = {
+    title: '',
+    description: '',
+  }
 
-  const translateY = visible ? '-100%' : '0%';
-  const examples = ExampleData.map(d => (
-    <Example
-      title={d.title}
-      description={d.description}
-      images={d.images}
-    />
-  ));
+  render() {
+    const { visible, onClose } = this.props;
+    const { title, description } = this.state;
 
-  return (
-    <div style={{
-      backgroundColor: Styles.colors[8],
-      width: '100%',
-      height: '100%',
-      overflow: 'scroll',
-      position: 'absolute',
-      top: '100%',
-      transition: 'transform .3s ease-in-out',
-      transform: `translateY(${translateY})`,
-      zIndex: 3,
-    }}
-    >
-      <a
-        id="close-examples-button"
-        href="#close-example"
-        onClick={onClose}
-        style={{
-          cursor: 'pointer',
-          color: Styles.colors[0],
-          position: 'absolute',
-          fontSize: 50,
-          right: 20,
-          top: 0,
+    const translateY = visible ? '-100%' : '0%';
+    const examples = ExampleData.map(d => (
+      <Example
+        images={d.images}
+        onHover={() => {
+          this.setState({
+            title: d.title,
+            description: d.description,
+          });
         }}
+      />
+    ));
+
+    return (
+      <div style={{
+        backgroundColor: Styles.colors[8],
+        width: '100%',
+        height: '100%',
+        overflow: 'scroll',
+        position: 'absolute',
+        top: '100%',
+        transition: 'transform .3s ease-in-out',
+        transform: `translateY(${translateY})`,
+        zIndex: 3,
+      }}
       >
-        &times;
-      </a>
-      {examples}
-    </div>
-  );
+        <a
+          id="close-examples-button"
+          href="#close-example"
+          onClick={onClose}
+          style={{
+            cursor: 'pointer',
+            color: Styles.colors[8],
+            backgroundColor: Styles.colors[0],
+            position: 'absolute',
+            fontSize: 50,
+            right: 0,
+            top: 20,
+            zIndex: 1,
+            padding: '0px 20px',
+            border: `5px solid ${Styles.colors[8]}`,
+            borderRight: 'none',
+          }}
+        >
+          &times;
+        </a>
+        {examples}
+        <div
+          style={{
+            position: 'absolute',
+            width: '100%',
+            padding: 20,
+            borderTop: `5px solid ${Styles.colors[0]}`,
+            backgroundColor: Styles.colors[8],
+            color: Styles.colors[0],
+            bottom: 0,
+          }}
+        >
+          <h3 style={{ color: Styles.colors[0] }}>{title}</h3>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Examples;
