84802b6db956196c9f61bd08fbacec3291a63e1f
Joe Longstreet <joelongstreet@gmail.com>
2019-05-19 12:23:11 -0500

    show placeholder text in datum tab

--- src/Settings-datum.js
+++ src/Settings-datum.js
@@ -35,100 +35,91 @@ function calculateTilePreviewPosition(e, context) {
   context.setTextAreaHighlightIndex(line);
 }
 
-const defaultSelectValue = random(0, ExampleData.length - 1);
+function SettingsDatum() {
+  return (
+    <SettingsContext.Consumer>
+      {context => (
+        <React.Fragment>
+          <Tooltip title="Load an example">
+            <Divider>Example</Divider>
+          </Tooltip>
+          <Select
+            placeholder="Change Example"
+            style={{ width: '100%' }}
+            onChange={val => handleExampleChange(val, context)}
+          >
+            {examplesOptions}
+          </Select>
 
-class SettingsDatum extends Component {
-  componentDidMount() {
-    handleExampleChange(defaultSelectValue, this.context);
-  }
+          <Tooltip title="Control the interpretation of the data">
+            <Divider style={Styles.divider}>Interpret</Divider>
+          </Tooltip>
 
-  render() {
-    return (
-      <SettingsContext.Consumer>
-        {context => (
-          <React.Fragment>
-            <Tooltip title="Load an example">
-              <Divider>Example</Divider>
-            </Tooltip>
-            <Select
-              defaultValue={defaultSelectValue}
-              style={{ width: '100%' }}
-              onChange={val => handleExampleChange(val, context)}
-            >
-              {examplesOptions}
-            </Select>
-
-            <Tooltip title="Control the interpretation of the data">
-              <Divider style={Styles.divider}>Interpret</Divider>
-            </Tooltip>
-
-            <Row style={{ marginBottom: 20 }}>
-              <Col span={12}>
-                <Radio.Group
-                  value={context.state.effectType}
-                  onChange={e => context.setEffectType(e.target.value)}
+          <Row style={{ marginBottom: 20 }}>
+            <Col span={12}>
+              <Radio.Group
+                value={context.state.effectType}
+                onChange={e => context.setEffectType(e.target.value)}
+              >
+                <Tooltip title="Draw the data as an etch">
+                  <Radio.Button value="etch">Etch</Radio.Button>
+                </Tooltip>
+                <Tooltip title="Draw the data as a cut">
+                  <Radio.Button value="cut">Cut</Radio.Button>
+                </Tooltip>
+              </Radio.Group>
+            </Col>
+            <Col span={12}>
+              <Tooltip title="Draw all data rows from the same origin">
+                <Checkbox
+                  style={{ float: 'right' }}
+                  checked={context.state.isSingleton}
+                  onChange={e => context.setIsSingleton(e.target.checked)}
                 >
-                  <Tooltip title="Draw the data as an etch">
-                    <Radio.Button value="etch">Etch</Radio.Button>
-                  </Tooltip>
-                  <Tooltip title="Draw the data as a cut">
-                    <Radio.Button value="cut">Cut</Radio.Button>
-                  </Tooltip>
-                </Radio.Group>
-              </Col>
-              <Col span={12}>
-                <Tooltip title="Draw all data rows from the same origin">
-                  <Checkbox
-                    style={{ float: 'right' }}
-                    checked={context.state.isSingleton}
-                    onChange={e => context.setIsSingleton(e.target.checked)}
-                  >
-                    Overlap
-                  </Checkbox>
-                </Tooltip>
-              </Col>
-            </Row>
+                  Overlap
+                </Checkbox>
+              </Tooltip>
+            </Col>
+          </Row>
 
-            <UnitSlider
-              label="Etch"
-              onChange={context.setEtchWidth}
-              value={context.state.etchWidth}
-              min={context.state.etchWidthMin}
-              step={0.01}
-              max={context.state.etchWidthMax}
-              disabled={context.state.effectType !== 'etch'}
-            />
+          <UnitSlider
+            label="Etch"
+            onChange={context.setEtchWidth}
+            value={context.state.etchWidth}
+            min={context.state.etchWidthMin}
+            step={0.01}
+            max={context.state.etchWidthMax}
+            disabled={context.state.effectType !== 'etch'}
+          />
 
-            <Tooltip title="Enter data">
-              <Divider style={Styles.divider}>Entry</Divider>
-            </Tooltip>
-            <Row>
-              <Col>
-                <Tooltip title="Add zeros between each data point">
-                  <Checkbox
-                    style={{ float: 'right', marginBottom: 10 }}
-                    checked={context.state.isDramatic}
-                    onChange={e => context.setIsDramatic(e.target.checked)}
-                  >
-                    Dramatize
-                  </Checkbox>
-                </Tooltip>
-              </Col>
-            </Row>
-            <TextArea
-              rows={10}
-              onClick={e => calculateTilePreviewPosition(e, context)}
-              onKeyUp={e => calculateTilePreviewPosition(e, context)}
-              value={context.state.datum}
-              style={{ fontFamily: 'monospace', whiteSpace: 'nowrap' }}
-              onChange={e => context.setDatum(e.target.value)}
-            />
-          </React.Fragment>
-        )}
-      </SettingsContext.Consumer>
-    );
-  }
+          <Tooltip title="Enter data">
+            <Divider style={Styles.divider}>Entry</Divider>
+          </Tooltip>
+          <Row>
+            <Col>
+              <Tooltip title="Add zeros between each data point">
+                <Checkbox
+                  style={{ float: 'right', marginBottom: 10 }}
+                  checked={context.state.isDramatic}
+                  onChange={e => context.setIsDramatic(e.target.checked)}
+                >
+                  Dramatize
+                </Checkbox>
+              </Tooltip>
+            </Col>
+          </Row>
+          <TextArea
+            rows={10}
+            onClick={e => calculateTilePreviewPosition(e, context)}
+            onKeyUp={e => calculateTilePreviewPosition(e, context)}
+            value={context.state.datum}
+            style={{ fontFamily: 'monospace', whiteSpace: 'nowrap' }}
+            onChange={e => context.setDatum(e.target.value)}
+          />
+        </React.Fragment>
+      )}
+    </SettingsContext.Consumer>
+  );
 }
 
-SettingsDatum.contextType = SettingsContext;
 export default SettingsDatum;
