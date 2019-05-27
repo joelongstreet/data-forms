554f975e46fcb3a913fec024e0e23304a32063a7
Joe Longstreet <joelongstreet@gmail.com>
2019-05-27 17:02:21 -0500

    Updates

---
+++ precache-manifest.a7534c1c24556b2e15546ee40e18c30c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "19e5a0754469bd0d6ca3",
+    "url": "/static/js/main.3a0827f9.chunk.js"
+  },
+  {
+    "revision": "f863c2fd4a517b56bbbd",
+    "url": "/static/js/2.d2341c8d.chunk.js"
+  },
+  {
+    "revision": "f863c2fd4a517b56bbbd",
+    "url": "/static/css/2.aa0b8b8d.chunk.css"
+  },
+  {
+    "revision": "3b65968ab2175a9ddac88b1fa6e0e712",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.344fce0666c47b7c04774d0f46db75c9.js"
+  "/precache-manifest.a7534c1c24556b2e15546ee40e18c30c.js"
 );
 
 workbox.clientsClaim();
